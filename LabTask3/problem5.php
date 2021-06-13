a8eb219ac679f007ce7dba835c71ceadeb6d4726
Arifur Rahman <arifurrahman1299@gmail.com>
2021-06-13 12:51:28 +0600

    Lab task 3

---
+++ LabTask3/problem5.php
@@ -0,0 +1,11 @@
+<?php
+    $backColor= "green";
+	$fontColor="red"; 
+	$imageWidth="300px"; 
+	$imageHeight="300px";
+?>
+
+    <img src="aiub_logo.jpg" width="<?php echo $imageWidth;?>" height="<?php echo $imageheight;?>" />
+    <h2>American International University -<span style="background-color:<?php echo $backColor;?>;color:<?php echo $fontColor;?>">
+    Bangladesh</h2>
+</span>
