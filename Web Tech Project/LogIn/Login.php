2eecef126db46ae5e6ef6d9fa089472e544d6af8
Arifur Rahman <arifurrahman1299@gmail.com>
2021-06-22 13:55:02 +0600

    project form

---
+++ Web Tech Project/LogIn/Login.php
@@ -0,0 +1,42 @@
+<html>
+	<body>
+	<fieldset><legend><h1>Ticket Management System</h1></legend>
+	<form border>
+	<table align = "Right">
+	<tr>
+		<td>
+		Username
+		</td>
+		<td>
+			<input type="text">
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Password
+		</td>
+		<td>
+			<input type="password">
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<a href="Forget Pass\forgetpass.php">Forget Password</a>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" value="Log In">
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" value="Create Account">
+		</td>
+	</tr>
+	</table>	
+	<form>
+	</fieldset>
+	</body>
+
+</html>
---
+++ Web Tech Project/Tickets/Launch ticket.php
@@ -0,0 +1,72 @@
+<html>
+	<body>
+	<fieldset><legend><h1>Launch Ticket</h1></legend>
+	<form border>
+	<table align = "center">
+	<tr>
+		<td>
+		From
+		</td>
+		<td>
+			<input type="text" placeholder="Enter City">
+		</td>
+	</tr>
+	<tr>
+		<td>
+		To
+		</td>
+		<td>
+			<input type="text" placeholder="Enter City" >
+		</td>
+	</tr>
+	<tr>
+		<td>
+		To
+		</td>
+		<td>
+			<select>
+				<option selected disabled >Pick a date</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		To
+		</td>
+		<td>
+			<select align ="center">
+				<option selected disabled >Date of Return <b>(Optional)</b></option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<input type="submit" value="Search Buses">
+		</td>
+	</tr>
+	</table>	
+	<form>
+	</fieldset>
+	</body>
+
+</html>
